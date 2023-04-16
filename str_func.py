f6c2204ecdd2cb8d4a6b3ab2a02fc7befca0ea58
OlyaEf <efimovskikh@gmail.com>
2023-04-16 02:19:39 +0100

    fix str_func

--- str_func.py
+++ str_func.py
@@ -1,7 +1,11 @@
+user = input()
+
+
 def get_string_upper(string):
     """
     Функция, которая принимает на вход строку и возвращает ее со всеми заглавными буквами.
     """
+    string = user
     return string.upper()
 
 
@@ -10,6 +14,8 @@ def get_string_title(string):
     Функция, которая делает заглавными первые буквы каждого слова в строке,
     поступившей на вход функции.
     """
+    string = user
     return string.title()
+
 
 
